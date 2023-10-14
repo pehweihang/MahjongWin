24c0be396eaae0ad296182840fa7d773259e7909
Niflnir <shaunchenggy@gmail.com>
2023-10-14 16:14:22 +0800

    Added TryFrom to all special tile types and updated From for Tile

--- server/src/mahjong/tile.rs
+++ server/src/mahjong/tile.rs
@@ -3,30 +3,56 @@ pub enum Tile {
     Wan(i8),
     Suo(i8),
     Tong(i8),
-    Feng(FengType),
-    Dragon(DragonType),
-    Hua(u8, HuaColor),
-    Animal(AnimalType),
+    Feng(Option<FengType>),
+    Dragon(Option<DragonType>),
+    Hua(Option<HuaType>),
+    Animal(Option<AnimalType>),
+    Invalid()
 }
 
 impl From<i8> for Tile {
     fn from(value: i8) -> Self {
-        let v = value % 10; 
-        let col = value/10;
-        match col {
-            0 => Tile::Wan(v),
-            1 => Tile::Suo(v),
-            2 => Tile::Tong(v),
-            3 => Tile::Feng(v),
-            4 => Tile::Dragon(v),
-            5 => Tile::Feng(v),
-            
+        let tile_value = value % 10;
+        let tile_type = value / 10;
+
+        match tile_type {
+            0 => Tile::Wan(tile_value),
+            1 => Tile::Suo(tile_value),
+            2 => Tile::Tong(tile_value),
+            3 => Tile::Feng(match FengType::try_from(tile_value) {
+                Ok(feng_type) => Some(feng_type),
+                Err(e) => {
+                    println!("Err: {}", e);
+                    None
+                }
+            }),
+            4 => Tile::Dragon(match DragonType::try_from(tile_value) {
+                Ok(dragon_type) => Some(dragon_type),
+                Err(e) => {
+                    println!("Err: {}", e);
+                    None
+                }
+            }),
+            5 => Tile::Hua(match HuaType::try_from(tile_value) {
+                Ok(hua_type) => Some(hua_type),
+                Err(e) => {
+                    println!("Err: {}", e);
+                    None
+                }
+            }),
+            6 => Tile::Animal(match AnimalType::try_from(tile_value) {
+                Ok(animal_type) => Some(animal_type),
+                Err(e) => {
+                    println!("Err: {}", e);
+                    None
+                }
+            }),
+            _ => Tile::Invalid()
         }
     }
-
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)] 
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum FengType {
     East = 1,
     South = 2,
@@ -35,39 +61,89 @@ pub enum FengType {
 }
 
 impl TryFrom<i8> for FengType {
+    type Error = String;
+
     fn try_from(value: i8) -> Result<Self, Self::Error> {
         match value {
             1 => Ok(FengType::East),
             2 => Ok(FengType::South),
             3 => Ok(FengType::West),
             4 => Ok(FengType::North),
-            x => Err(format!("Cannot convert {} into FengType", x))
-            
+            invalid_value => Err(format!("Cannot convert {} into FengType", invalid_value)),
         }
     }
 }
 
-
-
-#[derive(Debug, PartialEq, Eq, Hash)] 
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum DragonType {
     Zhong = 1,
     Baiban = 2,
-    Fa = 3
+    Fa = 3,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)] 
-pub enum HuaColor {
-    Red = 1,
-    Blue = 2,
+impl TryFrom<i8> for DragonType {
+    type Error = String;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(DragonType::Fa),
+            2 => Ok(DragonType::Zhong),
+            3 => Ok(DragonType::Baiban),
+            invalid_value => Err(format!("Cannot convert {} into DragonType", invalid_value)),
+        }
+    }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)] 
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum HuaType {
+    RedOne = 1,
+    RedTwo = 2,
+    RedThree = 3,
+    RedFour = 4,
+    BlueOne = 5,
+    BlueTwo = 6,
+    BlueThree = 7,
+    BlueFour = 8,
+}
+
+impl TryFrom<i8> for HuaType {
+    type Error = String;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(HuaType::RedOne),
+            2 => Ok(HuaType::RedTwo),
+            3 => Ok(HuaType::RedThree),
+            4 => Ok(HuaType::RedFour),
+            5 => Ok(HuaType::BlueOne),
+            6 => Ok(HuaType::BlueTwo),
+            7 => Ok(HuaType::BlueThree),
+            8 => Ok(HuaType::BlueFour),
+            invalid_value => Err(format!("Cannot convert {} into HuaColor", invalid_value)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum AnimalType {
     Cat = 1,
     Rat = 2,
     Rooster = 3,
     Centipede = 4,
+}
+
+impl TryFrom<i8> for AnimalType {
+    type Error = String;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(AnimalType::Cat),
+            2 => Ok(AnimalType::Rat),
+            3 => Ok(AnimalType::Rooster),
+            4 => Ok(AnimalType::Centipede),
+            invalid_value => Err(format!("Cannot convert {} into AnimalType", invalid_value)),
+        }
+    }
 }
 
 #[derive(Debug)]
