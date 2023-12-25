603bb400d6d84397dd9188e185f79c5ed6a988d5
weihang <pehweihang@live.com>
2023-12-26 02:57:11 +0800

    error handling

--- mahjong/src/meld.rs
+++ mahjong/src/meld.rs
@@ -1,51 +1,58 @@
-use crate::tile::{Suit, Tile};
+use crate::{
+    error::MahjongError,
+    tile::{Suit, Tile},
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Meld {
-    pub tiles: Vec<Tile>,
-    pub meld_type: MeldType,
-    pub suit: Suit,
+    tiles: Vec<Tile>,
+    meld_type: MeldType,
+    suit: Suit,
 }
 
 impl Meld {
-    pub fn new(mut tiles: Vec<Tile>, meld_type: MeldType) -> Result<Self, String> {
-        let suit = tiles.first().ok_or("No tiles to meld.")?.suit();
+    pub fn new(mut tiles: Vec<Tile>, meld_type: MeldType) -> Result<Self, MahjongError> {
+        let suit = tiles
+            .first()
+            .ok_or(MahjongError::InvalidMeldError(tiles.clone()))?
+            .suit();
         if suit == Suit::Animal || suit == Suit::Flower {
-            return Err(format!("Cannot meld tile of suit {:?}", suit));
+            return Err(MahjongError::InvalidMeldError(tiles));
         }
         match meld_type {
             MeldType::Chi => {
-                if suit != Suit::Wan || suit != Suit::Suo || suit != Suit::Tong {
-                    return Err(format!("Cannot Chi suit {:?}", suit));
+                if (suit != Suit::Wan && suit != Suit::Suo && suit != Suit::Tong)
+                    || tiles.len() != 3
+                    || !tiles.iter().all(|t| t.suit() == suit)
+                {
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
-                if tiles.len() != 3 {
-                    return Err(format!("Cannot Chi {} tiles", tiles.len()));
-                }
+
                 tiles.sort();
                 if !tiles.windows(2).all(|w| {
                     if let Some(x) = w[0].next() {
-                        x == w[1]
+                        x == w[1] && x.suit() == suit
                     } else {
                         false
                     }
                 }) {
-                    return Err(format!("Tiles {:?} is not a sequence", tiles));
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
             }
             MeldType::Pong => {
                 if tiles.len() != 3 {
-                    return Err(format!("Cannot Pong {} tiles", tiles.len()));
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
                 if !tiles.windows(2).all(|w| w[0] == w[1]) {
-                    return Err(format!("Can only Gang sets of same tiles, got {:?}", tiles));
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
             }
             MeldType::Gang | MeldType::AnGang => {
                 if tiles.len() != 4 {
-                    return Err(format!("Cannot Gang {} tiles", tiles.len()));
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
                 if !tiles.windows(2).all(|w| w[0] == w[1]) {
-                    return Err(format!("Can only Gang sets of same tiles, got {:?}", tiles));
+                    return Err(MahjongError::InvalidMeldError(tiles));
                 }
             }
         }
@@ -58,7 +65,7 @@ impl Meld {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum MeldType {
     Chi,
     Pong,
